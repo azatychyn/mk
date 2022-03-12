164a7f2e051f84161f61d6ff45cc5bbd6af5ed02
David Ter-Grigoryan <haxttanak@gmail.com>
2022-03-12 12:10:27 +0300

    4 commit

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :mk, MkWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  # url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true, # critical for Phoenix to run
   root: ".",
